44a975696eeb4928a1b96f13da4f998c10624cac
Gerson Marroquin <gerson.marroquin@gmail.com>
2020-07-28 10:47:04 -0700

    added inquirer function. question prompted with choices.

--- index.js
+++ index.js
@@ -6,6 +6,56 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "TheC@micGuy1985",
   database: "employeesDB",
 });
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  firstPrompt();
+});
+
+// function which prompts the user for what action they should take
+function firstPrompt() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View Employees",
+        "View Employees by Department",
+        "Add Employee",
+        "Remove Employees",
+        "Update Employee Role",
+        "Add Role",
+        "End",
+      ],
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View Employees":
+          viewEmployee();
+          break;
+        case "View Employees by Department":
+          viewEmployeeByDepartment();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Remove Employees":
+          removeEmployees();
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "End":
+          connection.end();
+          break;
+      }
+    });
+}
